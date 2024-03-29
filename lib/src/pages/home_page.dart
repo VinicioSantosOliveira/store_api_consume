a049dab401f060cb28ca6bdbb1aeaf022dce3fce
vinicio <viniciodossantosoliveira@hotmail.com>
2024-03-29 10:16:17 -0300

    teste

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Container(),
+      ),
+    );
   }
 }
